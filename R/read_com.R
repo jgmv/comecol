a2a98b57ec7a71241fa6b3eea1ad2d4eb64efd6c
jgmv <jg.macia@gmail.com>
2020-06-14 17:16:36 +0200

    2020-06-14

--- R/read_com.R
+++ R/read_com.R
@@ -5,9 +5,10 @@
 #' @param env the name of the file with a table with environmental data. First column must include sample names as in rows of 'cdm'.
 #' @param tax the name of the file with a table with taxonomic information. First column must include species names as in columns of 'cdm'.
 #' @param seq the name of the file where sequences for each species in fasta format. Sequence headers must be the species names as in columns of 'cdm'.
-#' @param phy the name of the file with a phylogenetic tree in Newick format including the species in 'cdm'. Tip labels must be the species names as in columns of 'cdm'. If no tree is provided, a taxonomic tree is automatically generated using the information in 'tax'.
+#' @param phy the name of the file with a phylogenetic tree in Newick format including the species in 'cdm'. Tip labels must be the species names as in columns of 'cdm'. If no tree is provided, a taxonomic tree can be automatically generated using the information in 'tax', using option 'tree_from_tax = T'.
 #' @param file the name of the file where the data will be stored as 'RData' object.
 #' @param use_saved read data from 'file' instead of importing them from scratch.
+#' @param tree_from_tax calculate hierarchical taxonomic tree from object tax.
 #' @param ... further arguments to be passed to 'read.table'.
 #' @details If no phylogenetic tree is provided, the calculation from the taxonomic data provided is performed using the perl script 'taxonomy_to_tree.pl' available at 'https://github.com/mr-y/my_bioinfPerls'.
 #' @return A list with community data.
@@ -16,7 +17,7 @@
 #' @examples
 #' read_com()
 read_com <- function(cdm, env, tax, seq = NULL, phy = NULL, file = NULL,
-  use_saved = F, ...) {  
+  use_saved = F, tree_from_tax = T, ...) {  
 
   if (use_saved & !file.exists(file)) message("No data, reading objects...")
   if(use_saved & file.exists(file)){    
@@ -34,7 +35,7 @@ read_com <- function(cdm, env, tax, seq = NULL, phy = NULL, file = NULL,
     
     # combine data
     com <- list(cdm = cdm, env = env, tax = tax, seq = seq, phy = phy) 
-    if(is.null(phy)) com$phy <- taxtotree(com) 
+    if(is.null(phy) & tree_from_tax) com$phy <- taxtotree(com) 
   
     # check data consistency
     com <- check_com(com)
