bec7a65df0f13fba43089b843693951236e5cfdd
jgmv <jg.macia@gmail.com>
2020-09-21 21:08:41 +0200

    2020-09-21

--- R/taxtotree.R
+++ R/taxtotree.R
@@ -2,13 +2,16 @@
 #'
 #' Generates a tree from the taxonomy table in a 'com' object.
 #' @param tax a taxonomy data frame.
+#' @param a first column to be included.
+#' @param b last column to be included.
 #' @return A phylogenetic tree in 'ape' format.
 #' @keywords community data
 #' @export
 #' @examples
 #' taxtotree()
-taxtotree <- function(tax) {
-
+taxtotree <- function(tax, a = 1, b = 7) {
+  
+  tax <- tax[, a:b]
   string <- tidyr::unite(cbind(tax, rownames(tax)), pathString, sep = "/")
   phy <- ape::as.phylo(data.tree::as.Node(string))
   return(phy)
